20a358cac02cc59d869ec31880b5c0f1f25ed3bd
Michael Kleen <michael.kleen@crate.io>
2020-04-09 15:42:16 +0200

    WIP

--- sql/src/main/java/io/crate/execution/dml/upsert/TransportShardInsertAction.java
+++ sql/src/main/java/io/crate/execution/dml/upsert/TransportShardInsertAction.java
@@ -22,24 +22,58 @@
 
 package io.crate.execution.dml.upsert;
 
+import com.google.common.annotations.VisibleForTesting;
 import io.crate.execution.ddl.SchemaUpdateClient;
+import io.crate.execution.dml.ShardResponse;
+import io.crate.execution.dml.TransportShardAction;
+import io.crate.execution.dml.upsert.ShardWriteRequest.DuplicateKeyAction;
 import io.crate.execution.jobs.TasksService;
 import io.crate.metadata.Functions;
+import io.crate.metadata.Reference;
+import io.crate.metadata.RelationName;
 import io.crate.metadata.Schemas;
+import io.crate.metadata.TransactionContext;
+import io.crate.metadata.doc.DocTableInfo;
+import io.crate.metadata.table.Operation;
+import org.elasticsearch.ExceptionsHelper;
+import org.elasticsearch.action.support.replication.TransportReplicationAction;
 import org.elasticsearch.cluster.action.shard.ShardStateAction;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.service.ClusterService;
+import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.inject.Singleton;
+import org.elasticsearch.common.lucene.uid.Versions;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.VersionType;
+import org.elasticsearch.index.engine.Engine;
+import org.elasticsearch.index.engine.VersionConflictEngineException;
+import org.elasticsearch.index.mapper.SourceToParse;
+import org.elasticsearch.index.seqno.SequenceNumbers;
+import org.elasticsearch.index.shard.IndexShard;
+import org.elasticsearch.index.translog.Translog;
 import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static io.crate.exceptions.SQLExceptions.userFriendlyCrateExceptionTopOnly;
+
+
 @Singleton
-public class TransportShardInsertAction extends
-        TransportShardWriteAction<ShardInsertRequest, ShardInsertRequest.Item> {
+public class TransportShardInsertAction extends TransportShardAction<ShardInsertRequest, ShardInsertRequest.Item> {
 
     private static final String ACTION_NAME = "internal:crate:sql/data/insert";
+    private static final int MAX_RETRY_LIMIT = 100_000; // upper bound to prevent unlimited retries on unexpected states
+
+    private final Schemas schemas;
+    private final Functions functions;
 
     @Inject
     public TransportShardInsertAction(
@@ -52,20 +86,228 @@ public class TransportShardInsertAction extends
         ShardStateAction shardStateAction,
         Functions functions,
         Schemas schemas,
-        IndexNameExpressionResolver indexNameExpressionResolver) {
+        IndexNameExpressionResolver indexNameExpressionResolver
+    ) {
         super(
             ACTION_NAME,
-            threadPool,
-            clusterService,
             transportService,
-            schemaUpdateClient,
-            tasksService,
-            indicesService,
-            shardStateAction,
-            functions,
-            schemas,
             indexNameExpressionResolver,
-            ShardInsertRequest::new
+            clusterService,
+            indicesService,
+            threadPool,
+            shardStateAction,
+            ShardInsertRequest::new,
+            schemaUpdateClient
         );
+        this.schemas = schemas;
+        this.functions = functions;
+        tasksService.addListener(this);
+    }
+
+    @Override
+    protected WritePrimaryResult<ShardInsertRequest, ShardResponse> processRequestItems(IndexShard indexShard,
+                                                                                        ShardInsertRequest request,
+                                                                                        AtomicBoolean killed) {
+        ShardResponse shardResponse = new ShardResponse();
+        String indexName = request.index();
+        DocTableInfo tableInfo = schemas.getTableInfo(RelationName.fromIndexName(indexName), Operation.INSERT);
+        Reference[] insertColumns = request.insertColumns();
+        GeneratedColumns.Validation valueValidation = request.validateConstraints()
+            ? GeneratedColumns.Validation.VALUE_MATCH
+            : GeneratedColumns.Validation.NONE;
+
+        TransactionContext txnCtx = TransactionContext.of(request.sessionSettings());
+        InsertSourceGen insertSourceGen = insertColumns == null
+            ? null
+            : InsertSourceGen.of(txnCtx, functions, tableInfo, indexName, valueValidation, Arrays.asList(insertColumns));
+
+
+        Translog.Location translogLocation = null;
+        for (ShardInsertRequest.Item item : request.items()) {
+            int location = item.location();
+            if (killed.get()) {
+                // set failure on response and skip all next items.
+                // this way replica operation will be executed, but only items with a valid source (= was processed on primary)
+                // will be processed on the replica
+                shardResponse.failure(new InterruptedException());
+                break;
+            }
+            try {
+                Translog.Location translog = indexItem(
+                    request,
+                    item,
+                    indexShard,
+                    insertSourceGen
+                );
+                    if (translog != null) {
+                        shardResponse.add(location);
+                        translogLocation = translog;
+                    }
+            } catch (Exception e) {
+                if (retryPrimaryException(e)) {
+                    if (e instanceof RuntimeException) {
+                        throw (RuntimeException) e;
+                    }
+                    throw new RuntimeException(e);
+                }
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Failed to execute upsert shardId={} id={} error={}", request.shardId(), item.id(), e);
+                }
+
+                // *mark* the item as failed by setting the source to null
+                // to prevent the replica operation from processing this concrete item
+                item.source(null);
+
+                if (!request.continueOnError()) {
+                    shardResponse.failure(e);
+                    break;
+                }
+                shardResponse.add(location,
+                                  new ShardResponse.Failure(
+                                      item.id(),
+                                      userFriendlyCrateExceptionTopOnly(e),
+                                      (e instanceof VersionConflictEngineException)));
+            }
+        }
+        return new WritePrimaryResult<>(request, shardResponse, translogLocation, null, indexShard);
+    }
+
+    @Override
+    protected WriteReplicaResult<ShardInsertRequest> processRequestItemsOnReplica(IndexShard indexShard, ShardInsertRequest request) throws IOException {
+        Translog.Location location = null;
+        for (ShardInsertRequest.Item item : request.items()) {
+            if (item.source() == null) {
+                continue;
+            }
+            SourceToParse sourceToParse = new SourceToParse(
+                indexShard.shardId().getIndexName(),
+                item.id(),
+                item.source(),
+                XContentType.JSON
+            );
+
+            Engine.IndexResult indexResult = indexShard.applyIndexOperationOnReplica(
+                item.seqNo(),
+                item.version(),
+                Translog.UNSET_AUTO_GENERATED_TIMESTAMP,
+                false,
+                sourceToParse
+            );
+            if (indexResult.getResultType() == Engine.Result.Type.MAPPING_UPDATE_REQUIRED) {
+                // Even though the primary waits on all nodes to ack the mapping changes to the master
+                // (see MappingUpdatedAction.updateMappingOnMaster) we still need to protect against missing mappings
+                // and wait for them. The reason is concurrent requests. Request r1 which has new field f triggers a
+                // mapping update. Assume that that update is first applied on the primary, and only later on the replica
+                // (it’s happening concurrently). Request r2, which now arrives on the primary and which also has the new
+                // field f might see the updated mapping (on the primary), and will therefore proceed to be replicated
+                // to the replica. When it arrives on the replica, there’s no guarantee that the replica has already
+                // applied the new mapping, so there is no other option than to wait.
+                throw new TransportReplicationAction.RetryOnReplicaException(indexShard.shardId(),
+                                                                             "Mappings are not available on the replica yet, triggered update: " + indexResult.getRequiredMappingUpdate());
+            }
+            location = indexResult.getTranslogLocation();
+        }
+        return new WriteReplicaResult<>(request, location, null, indexShard);
+    }
+
+    @Nullable
+    private Translog.Location indexItem(ShardInsertRequest request,
+                                        ShardInsertRequest.Item item,
+                                        IndexShard indexShard,
+                                        InsertSourceGen insertSourceGen) throws Exception {
+        VersionConflictEngineException lastException = null;
+        boolean isRetry;
+        for (int retryCount = 0; retryCount < MAX_RETRY_LIMIT; retryCount++) {
+            try {
+                isRetry = retryCount > 0;
+                return insert(request, item, indexShard, isRetry, insertSourceGen);
+            } catch (VersionConflictEngineException e) {
+                lastException = e;
+                if (request.duplicateKeyAction() == DuplicateKeyAction.IGNORE) {
+                    // on conflict do nothing
+                    item.source(null);
+                    return null;
+                }
+            }
+        }
+        logger.warn("[{}] VersionConflict for document id={}, version={} exceeded retry limit of {}, will stop retrying",
+                    indexShard.shardId(), item.id(), item.version(), MAX_RETRY_LIMIT);
+        throw lastException;
+    }
+
+    @VisibleForTesting
+    protected Translog.Location insert(ShardInsertRequest request,
+                                       ShardInsertRequest.Item item,
+                                       IndexShard indexShard,
+                                       boolean isRetry,
+                                       InsertSourceGen insertSourceGen) throws Exception {
+        assert insertSourceGen != null : "InsertSourceGen must not be null";
+        BytesReference rawSource;
+        Map<String, Object> source = null;
+        try {
+            // This optimizes for the case where the insert value is already string-based, so we can take directly
+            // the rawSource
+            if (insertSourceGen instanceof FromRawInsertSource) {
+                rawSource = insertSourceGen.generateSourceAndCheckConstraintsAsBytesReference(item.insertValues());
+            } else {
+                source = insertSourceGen.generateSourceAndCheckConstraints(item.insertValues());
+                rawSource = BytesReference.bytes(XContentFactory.jsonBuilder().map(source));
+            }
+        } catch (IOException e) {
+            throw ExceptionsHelper.convertToElastic(e);
+        }
+        item.source(rawSource);
+
+        long version = request.duplicateKeyAction() == DuplicateKeyAction.OVERWRITE ? Versions.MATCH_ANY : Versions.MATCH_DELETED;
+        long seqNo = SequenceNumbers.UNASSIGNED_SEQ_NO;
+        long primaryTerm = SequenceNumbers.UNASSIGNED_PRIMARY_TERM;
+
+        Engine.IndexResult indexResult = index(item, indexShard, isRetry, seqNo, primaryTerm, version);
+        return indexResult.getTranslogLocation();
+    }
+
+    private Engine.IndexResult index(ShardInsertRequest.Item item,
+                                     IndexShard indexShard,
+                                     boolean isRetry,
+                                     long seqNo,
+                                     long primaryTerm,
+                                     long version) throws Exception {
+        SourceToParse sourceToParse = new SourceToParse(
+            indexShard.shardId().getIndexName(),
+            item.id(),
+            item.source(),
+            XContentType.JSON
+        );
+
+        Engine.IndexResult indexResult = executeOnPrimaryHandlingMappingUpdate(
+            indexShard.shardId(),
+            () -> indexShard.applyIndexOperationOnPrimary(
+                version,
+                VersionType.INTERNAL,
+                sourceToParse,
+                seqNo,
+                primaryTerm,
+                Translog.UNSET_AUTO_GENERATED_TIMESTAMP,
+                isRetry
+            ),
+            e -> indexShard.getFailedIndexResult(e, Versions.MATCH_ANY)
+        );
+
+        switch (indexResult.getResultType()) {
+            case SUCCESS:
+                // update the seqNo and version on request for the replicas
+                item.seqNo(indexResult.getSeqNo());
+                item.version(indexResult.getVersion());
+                return indexResult;
+
+            case FAILURE:
+                Exception failure = indexResult.getFailure();
+                assert failure != null : "Failure must not be null if resultType is FAILURE";
+                throw failure;
+
+            case MAPPING_UPDATE_REQUIRED:
+            default:
+                throw new AssertionError("IndexResult must either succeed or fail. Required mapping updates must have been handled.");
+        }
     }
 }
